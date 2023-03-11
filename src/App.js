55855d103c9972e0829fa12daed9b512dec87322
MalMWare <malkelleymartin@gmail.com>
2023-03-10 23:45:36 -0800

    rendered nine mole images

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 //import logo from './logo.svg';
+import { useState } from 'react'
 import MoleContainer from './components/MoleContainer';
 import './App.css';
 
 function App() {
+  let [score, setScore] = useState(0)
+
+  const createMoleHill = () => {
+    let hills = []
+    for (let i = 0; i < 9; i++) {
+      hills.push(
+        <MoleContainer key={i}
+        setScore={setScore} score={score}
+      />)
+    }
+    return (
+      <div>
+        {hills}
+      </div>
+    )
+  }
   return (
     <div className='App'>
-      <MoleContainer />
+      <h1>React-A-Mole!</h1>
+      {score}
+      {createMoleHill()}
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
